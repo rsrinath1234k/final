0f1721e98e2dae137d1c691722cfc3474847467f
Dhruv Goel <dhruv.goel@kellogg.northwestern.edu>
2021-05-25 12:28:38 -0500

    built login system

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
     console.log('signed in')
-  } else {
+
+                                                                    //added the code below in here to test
+    // Build the markup for the sign-out button and set the HTML in the header
+    document.querySelector(`.sign-in-or-sign-out`).innerHTML = `
+      <button class="text-pink-500 underline sign-out">Sign Out</button>
+    `
+ // get a reference to the sign out button
+ let signOutButton = document.querySelector(`.sign-out`)
+
+ // handle the sign out button click
+ signOutButton.addEventListener(`click`, function(event) {
+   // sign out of firebase authentication
+   firebase.auth().signOut()
+
+   // redirect to the home page
+   document.location.href = `Voting Page.html`
+                                                                    //added the code above in here to test
+
+
+  })
+ } else {
     // Signed out
     console.log('signed out')
 
@@ -14,7 +34,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
       signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID
       ],
-      signInSuccessUrl: 'index.html'
+      signInSuccessUrl: 'Voting Page.html' // where to go after we're done signing up/in
     }
 
     // Starts FirebaseUI Auth
--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,15 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyCzxRwKdrNDr6m2J94AL4HavnZQI9SdIuE",
+  authDomain: "kiei-final-project-8da54.firebaseapp.com",
+  projectId: "kiei-final-project-8da54",
+  storageBucket: "kiei-final-project-8da54.appspot.com",
+  messagingSenderId: "874218534068",
+  appId: "1:874218534068:web:929d6b83ab39ea84ce53e1",
+  measurementId: "G-KBK6GDYJXM"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
