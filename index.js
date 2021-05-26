d6f359ba28d7327afddb0751674208e6528ae0e2
Raghav Srinath <raghav.srinath@kellogg.northwestern.com>
2021-05-26 13:51:08 -0500

    adding voting

--- index.js
+++ index.js
@@ -19,7 +19,30 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
    // redirect to the voting page
    document.location.href = `Voting Page.html`
+  
+    // get a reference to the "Vote" button
+    let postButton = document.querySelector(`#vote-button`)
 
+    // handle the clicking of the "Vote" button
+    postButton.addEventListener(`click`, async function(event) {
+      // prevent the default behavior (submitting the form)
+      event.preventDefault()
+
+      // get a reference to the input holding the vote URL
+      let voteUrlInput = document.querySelector(`#vote-url`)
+
+      // store the user-inputted vote URL in memory
+      let voteUrl = voteUrlInput.value
+
+      // create the URL for our "create post" lambda function
+      let url = `/.netlify/functions/create_vote?userName=${user.displayName}&imageUrl=${voteUrl}`
+
+      // fetch the URL, wait for the response, store the response in memory
+      let response = await fetch(url)
+
+      // refresh the page
+      location.reload()
+    })
 
   })
  } else {
