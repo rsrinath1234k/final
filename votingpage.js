01637c9bbdf74a91047bfea417dd69437d317092
Raghav Srinath <raghav.srinath@kellogg.northwestern.com>
2021-05-26 16:16:50 -0500

    updated vote button location

--- votingpage.js
+++ votingpage.js
@@ -52,29 +52,30 @@ window.addEventListener('DOMContentLoaded', async function() {
       `)
     }
 
-    // get a reference to the "Vote" button
-    let postButton = document.querySelector(`#vote-button`)
+    // // get a reference to the "Vote" button
+    // let postButton = document.querySelector(`#vote-button`)
 
-    // handle the clicking of the "Vote" button
-    postButton.addEventListener(`click`, async function(event) {
-      // prevent the default behavior (submitting the form)
-      event.preventDefault()
+    // // handle the clicking of the "Vote" button
+    // postButton.addEventListener(`click`, async function(event) {
+    //   // prevent the default behavior (submitting the form)
+      
+    //   event.preventDefault()
 
-      // get a reference to the input holding the vote URL
-      let voteUrlInput = document.querySelector(`#vote-url`)
+    //   // get a reference to the input holding the vote URL
+    //   let voteUrlInput = document.querySelector(`#vote-url`)
 
-      // store the user-inputted vote URL in memory
-      let voteUrl = voteUrlInput.value
+    //   // store the user-inputted vote URL in memory
+    //   let voteUrl = voteUrlInput.value
 
-      // create the URL for our "create post" lambda function
-      let url = `/.netlify/functions/create_vote?userName=${user.displayName}&imageUrl=${voteUrl}`
+    //   // create the URL for our "create post" lambda function
+    //   let url = `/.netlify/functions/create_vote?userName=${user.displayName}&imageUrl=${voteUrl}`
 
-      // fetch the URL, wait for the response, store the response in memory
-      let response = await fetch(url)
+    //   // fetch the URL, wait for the response, store the response in memory
+    //   let response = await fetch(url)
 
-      // refresh the page
-      location.reload()
-    })
+    //   // refresh the page
+    //   location.reload()
+    // })
     
     })
 
