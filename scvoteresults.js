dddbb1ef5dfc89f517e5508de41072ef55098814
Raghav Srinath <raghav.srinath@kellogg.northwestern.com>
2021-06-03 16:36:23 -0500

    formating

--- scvoteresults.js
+++ scvoteresults.js
@@ -34,7 +34,7 @@ window.addEventListener('DOMContentLoaded', async function() {
 
                   <ol class ="m-auto text-center" >
                       <li class="m-auto text-left text-white text-2xl">_</li> <!--this is essentially acting as white space-->
-                      <li class="m-auto text-left text-red-600 text-2xl">Stephen Curry: ${vote.scvotes}</li>
+                      <li class="m-auto text-left text-red-600 text-2xl">Stephen Curry Votes: ${vote.scvotes}</li>
 
                   </ol>
                   </div>
